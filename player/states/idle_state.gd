d9436a489334bec13c52f474bea4b69b9ad1511d
Steaf23 <steafmobile@gmail.com>
2024-06-21 23:11:47 +0200

    added walking animation to player

--- player/states/idle_state.gd
+++ player/states/idle_state.gd
@@ -2,10 +2,17 @@ extends State
 
 
 func do(delta: float) -> void:
+	var walking = machine.actor.controller.desired_velocity.length() > 20
 	if not machine.actor.rightside:
-		machine.actor.play_animation("idle_left")
+		if walking:
+			machine.actor.play_animation("walk_left")
+		else:
+			machine.actor.play_animation("idle_left")
 	else:
-		machine.actor.play_animation("idle_right")
+		if walking:
+			machine.actor.play_animation("walk_right")
+		else:
+			machine.actor.play_animation("idle_right")
 
 
 func attack(combo: int) -> void:
